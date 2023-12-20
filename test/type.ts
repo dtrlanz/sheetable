8a0b2d856346c4b4dfed0f0f613288f9b57a9a86
dtrlanz <68445623+dtrlanz@users.noreply.github.com>
2023-12-20 18:42:10 +0500

    implement recursive object creation

--- test/type.ts
+++ test/type.ts
@@ -1,5 +1,6 @@
 import test from 'ava';
-import { type, getPropConstructor, createFromEntries } from "../src/type.js";
+import { type, getPropConstructor, createFromEntries, createRecursively } from "../src/type.js";
+import { title } from '../src/title.js';
 
 test('from entries', t => {
     // without static factory method
@@ -82,4 +83,94 @@ test('get property constructor', t => {
     const e = new E();
     t.is(getPropConstructor(e, 'foo'), A);
     t.is(getPropConstructor(e, 'bar'), B, 'should prioritize @type value');
+});
+
+test('create nested objects recursively', t => {
+    class A {
+        a = 1;
+        b = 2;
+    }
+    class B {
+        x = 3;
+        y = 4;
+    }
+    // depth 1
+    const obj0 = createRecursively(A, [
+        [['a'], 10],
+        [['b'], 20],
+    ]);
+    t.deepEqual(obj0, Object.setPrototypeOf({ a: 10, b: 20 }, A.prototype));
+
+    class C {
+        foo = new A();
+        bar = new B();
+    }
+    // depth 2
+    const obj1 = createRecursively(C, [
+        [['foo', 'a'], 10],
+        [['foo', 'b'], 20],
+        [['bar', 'x'], 30],
+        [['bar', 'y'], 40],
+    ]);
+    t.deepEqual(obj1, Object.setPrototypeOf({
+        foo: Object.setPrototypeOf({ a: 10, b: 20}, A.prototype),
+        bar: Object.setPrototypeOf({ x: 30, y: 40}, B.prototype),
+    }, C.prototype));
+
+    class D {
+        foo = new A();
+        bar = [new B(), new B()];
+    }
+    // setting one property to an array
+    const obj2 = createRecursively(D, [
+        [['foo', 'a'], 10],
+        [['foo', 'b'], 20],
+        [['bar', 0, 'x'], 30],
+        [['bar', 0, 'y'], 40],
+        [['bar', 1, 'x'], 50],
+        [['bar', 1, 'y'], 60],
+    ]);
+    t.deepEqual(obj2, Object.setPrototypeOf({
+        foo: Object.setPrototypeOf({ a: 10, b: 20}, A.prototype),
+        bar: [
+            Object.setPrototypeOf({ x: 30, y: 40}, B.prototype), 
+            Object.setPrototypeOf({ x: 50, y: 60}, B.prototype)
+        ],
+    }, D.prototype));
+
+    const sym = Symbol('sym');
+    class E {
+        foo = new A();
+        bar = ['apples', 'bananas'];
+        [sym] = 'zygomorphic';
+    }
+    // using symbol as property key; mixing different depths
+    const obj3 = createRecursively(E, [
+        [['foo', 'a'], 10],
+        [['foo', 'b'], 20],
+        [['bar', 0], 'oranges'],
+        [['bar', 1], 'pears'],
+        [['bar', 2], 'carrots'],
+        [[sym], 'stupendous'],
+    ]);
+    t.deepEqual(obj3, Object.setPrototypeOf({
+        foo: Object.setPrototypeOf({ a: 10, b: 20}, A.prototype),
+        bar: ['oranges', 'pears', 'carrots'],
+        [sym]: 'stupendous',
+    }, E.prototype));
+
+    // using `Object` instead of a class
+    const obj4 = createRecursively(Object, [
+        [['foo', 'a'], 10],
+        [['foo', 'b'], 20],
+        [['bar', 0], 'oranges'],
+        [['bar', 1], 'pears'],
+        [['bar', 2], 'carrots'],
+        [[sym], 'stupendous'],
+    ]);
+    t.deepEqual(obj4, {
+        foo: { a: 10, b: 20},
+        bar: ['oranges', 'pears', 'carrots'],
+        [sym]: 'stupendous',
+    });
 });
