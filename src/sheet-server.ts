5619b141ce4cc72f0d1ba966707b702b5ea562e8
dtrlanz <68445623+dtrlanz@users.noreply.github.com>
2023-12-22 19:00:04 +0500

    clean up

--- src/sheet-server.ts
+++ src/sheet-server.ts
@@ -2,106 +2,6 @@ import { SheetLike, Orientation, Region, TableWalker } from "./sheet-navigation.
 import { Branch, getHeadersHelper } from "./headers.js";
 import { Sendable, toSendable } from "./values.js";
 
-export function getSheet(info: Sheetable.SheetInfo): { spreadsheet: Spreadsheet, sheet: Sheet, orientation: Sheetable.Orientation } {
-    let spreadsheet: Spreadsheet;
-    let sheet: Sheet;
-    if (!info.url && !info.id) {
-        spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-        if (!info.sheetName) {
-            sheet = spreadsheet.getActiveSheet();
-        }
-    } else {
-        if (info.url) {
-            spreadsheet = SpreadsheetApp.openByUrl(info.url)
-        } else {
-            spreadsheet = SpreadsheetApp.openById(info.id!);
-        }
-    }
-    if (info.sheetName) {
-        const s = spreadsheet.getSheetByName(info.sheetName);
-        if (!s) throw new Error(`Sheet named '${info.sheetName} does not exist.`);
-        sheet = s;
-    } else {
-        sheet = spreadsheet.getSheets()[0];
-    }
-    const orientation = info.orientation ?? 'normal';
-    return { spreadsheet, sheet, orientation };
-}
-
-export function getSheetData(info: Sheetable.SheetInfo = {}, rowStart: number, rowStop?: number, columnLabels?: string[]): Sheetable.SheetData {
-    const { spreadsheet, sheet, orientation } = getSheet(info);
-    const region = Sheetable.Region.fromSheet(sheet, orientation).resize(undefined, rowStop);
-    const branches = Sheetable.getHeadersForClient(new Sheetable.TableWalker(region));
-
-    // return all columns by default
-    columnLabels ??= branches.map(br => br.label);
-
-    const columnData: Sheetable.Sendable[][] = [];
-    for (const label of columnLabels) {
-        for (const br of branches) {
-            if (br.label === label) {
-                for (let col = br.start; col < br.stop; col++) {
-                    // note that column data will include header row(s)
-                    const walker = new Sheetable.TableWalker(region, rowStart, col)
-                    // use 0-indexed arrays for consistency
-                    columnData[col - 1] = walker.map(1, 0, scalarToSendable);
-                }
-                break;
-            }
-        }
-    }
-    
-    return {
-        url: spreadsheet.getUrl(),
-        sheetName: sheet.getName(),
-        orientation: orientation,
-        headers: branches,
-        columns: columnData,
-        rowOffset: rowStart,
-    }
-}
-
-export function getSheetColumns(info: Sheetable.SheetInfo = {}, columns: number[], rowStart: number, rowStop?: number): Sheetable.SheetColumns {
-    const { sheet, orientation } = getSheet(info);
-    const region = Sheetable.Region.fromSheet(sheet, orientation).resize(undefined, rowStop);
-
-    const columnData: Sheetable.Sendable[][] = [];
-    for (const col of columns) {
-        const walker = new Sheetable.TableWalker(region, rowStart, col)
-        // use 0-indexed arrays for consistency
-        columnData[col - 1] = walker.map(1, 0, scalarToSendable);
-    }
-    
-    return {
-        columns: columnData,
-        rowOffset: rowStart,
-    }
-}
-
-function writeSheetRow(info: Sheetable.SheetInfo, row: number, vals: Sheetable.Sendable[], checkState?: Sheetable.CellCheck) {
-    const arr = vals.map(v =>
-        typeof v !== 'object' ? v
-            : v === null ? null
-            : Sheetable.SENDABLE_DATE_KEY in v ? new Date(v[Sheetable.SENDABLE_DATE_KEY] as number)
-            : '[internal error at writeSheetRow]'
-    );
-    const { sheet, orientation } = getSheet(info);
-    const region = Sheetable.Region.fromSheet(sheet, orientation);
-    region.writeRow(row, arr, 'encroach');
-}
-
-function scalarToSendable(val: any): Sheetable.Sendable {
-    if (typeof val === 'string' ||
-        typeof val === 'number' ||
-        typeof val === 'boolean' ||
-        typeof val === 'undefined') 
-    {
-        return val;
-    } else if (Date.prototype.isPrototypeOf(val)) {
-        return (val as Date).getTime();
-    }
-}
-
 type SheetRequest = {
     orientation: Orientation,
     limit?: {
@@ -215,7 +115,7 @@ export class SheetServer {
                     // guessing it would be reasonably efficient. Another, possibly better
                     // option would be to write each column (or even each set of contiguous
                     // columns) in turn. But there's no point implementing that before I have
-                    // a system in place to benchmark it.
+                    // a system in place to benchmark it (and a use case!).
 
                     // 1. read existing data
                     const min = colNumbers.reduce((a, b) => a < b ? a : b);
