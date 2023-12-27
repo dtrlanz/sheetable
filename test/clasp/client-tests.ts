2ab39f9f21b24a33e6cda9c654ca6a1d3f36723a
dtrlanz <68445623+dtrlanz@users.noreply.github.com>
2023-12-27 15:48:41 +0500

    display test results

--- test/clasp/client-tests.ts
+++ test/clasp/client-tests.ts
@@ -1,23 +1,26 @@
 import { test } from "./test";
-export { display } from "./test";
 import { SF } from "./server-fns";
 import { server as _server, TypedServer } from "./server-proxy";
 
 const server = _server as TypedServer<SF>;
 
-test('my test', t => {
+test('passing test', t => {
     t.is(4, 4);
     t.assert(true);
     t.is(5, 5);
 });
 
-test('add', async t => {
+test('failing test', t => {
+    t.assert(0);
+});
+
+test('passing async test', async t => {
     const r = await server.add(1, 2);
     t.is(r, 3);
 });
 
-test('subtract', async t => {
+test('failing async test', async t => {
     const r = await server.subtract(1, 2);
-    t.is(r, -1);
+    t.is(r, 42);
 });
 
--- test/clasp/test.ts
+++ test/clasp/test.ts
@@ -1,24 +1,66 @@
 import diff from "microdiff";
 
-
-export function test(name: string, fn: (t: ExecutionContext) => void | Promise<void>) {
-    const t = new ExecutionContext();
-    (new Promise<void>(res => {
-        fn(t);
-        res();
-    })).catch(reason => {
-        console.error(reason);
-    });
-}
-
 let div: HTMLDivElement | undefined;
 
 export function display(elem?: HTMLDivElement | null) {
     div = elem ?? undefined;
+    console.log(div);
 }
 
+export function test(name: string, fn: (t: ExecutionContext) => void | Promise<void>) {
+    let output: HTMLDivElement | undefined;
+
+    setTimeout(run);
+
+    async function run() {
+        setStatus('pending');
+
+        const t = new ExecutionContext();
+
+        (async () => await fn(t))()
+        .then(() => setStatus('passed'))
+        .catch(error => {
+            setStatus('failed', error.message);
+            if (output) {
+                (output.getElementsByClassName('stack')[0] as HTMLDivElement).innerText = error.stack;
+                output.getElementsByClassName('values')[0].innerHTML = '<table>' +
+                    Object.entries(t.values).map(([k, v]) => 
+                        `<tr class="${k}">
+                            <th>${k}</th>
+                            <td>${v}</td>
+                        </tr>`
+                    ).join('') + '</table>';
+            }
+            console.error(error);
+        });
+    }
+
+    function setStatus(status: 'pending' | 'passed' | 'failed', message?: string) {
+        if (div && !output) {
+            output = document.createElement('div');
+            output.classList.add('test');
+            output.innerHTML = `<h1>${name}</h1>
+                <div class="result"></div>
+                <div class="values"></div>
+                <div class="stack"></div>`;
+            output.getElementsByTagName('h1')[0]
+                .addEventListener('click', () => output?.classList.toggle('collapsed'));
+            div.appendChild(output);
+        }
+        if (!output) return;
+        output.classList.remove('pending', 'passed', 'failed', 'collapsed');
+        output.classList.add(status);
+        if (status === 'passed') output.classList.add('collapsed');
+        output.getElementsByClassName('result')[0].textContent = message ?? status;
+    }
+}
+
+
 class ExecutionContext {
+    values = {};
+
     assert(actual: any, message?: string) {
+        this.values = { actual };
         message ??= 'Assertion failed';
         if (!actual) {
             throw new Error(`${message}\nvalue: ${actual}`);
@@ -26,9 +68,10 @@ class ExecutionContext {
     }
 
     is(actual: any, expected: any, message?: string) {
+        this.values = { actual, expected };
         message ??= 'Equality assertion failed';
         if (actual !== expected) {
-            throw new Error(`${message}'\nactual: ${actual}\nexpected: ${expected}`);
+            throw new Error(`${message}\nactual: ${actual}\nexpected: ${expected}`);
         }
     }
 }
