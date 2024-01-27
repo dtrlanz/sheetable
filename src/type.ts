db3a47154ff3218140fe6ec7be37b42c7a9a6de5
dtrlanz <68445623+dtrlanz@users.noreply.github.com>
2024-01-27 15:09:17 +0500

    implement default property config for UI
    not tested yet

--- src/type.ts
+++ src/type.ts
@@ -1,7 +1,14 @@
 import { MetaProperty, Constructor } from "./meta-props.js";
 
-type Type = 'string' | 'number' | 'bigint' | 'boolean' | 'symbol'
-    | Constructor;
+type Type = Constructor
+    | typeof String
+    | typeof Number
+    | typeof BigInt
+    | typeof Boolean
+    | typeof Symbol
+    | typeof Date
+    | undefined
+    | Type[];
 
 const typeProp = new MetaProperty<Type>('type');
 
@@ -9,7 +16,7 @@ export function type(t: Type) {
     return typeProp.getDecorator(t);
 }
 
-export function getPropConstructor(obj: object | Constructor, key: string | symbol, context?: { [k: string]: any }): Constructor | undefined {
+export function getPropType(obj: object | Constructor, key: string | symbol, context?: { [k: string]: any }): Type {
     // TODO: cache this stuff
 
     // If the property type is specified with @type, use that
@@ -30,13 +37,194 @@ export function getPropConstructor(obj: object | Constructor, key: string | symb
     let val = (obj as any)[key];
     
     // in case of array, use first element as sample
+    let arr = false;
     if (Array.isArray(val)) {
         val = val[0];
+        arr = true;
     }
 
-    if (val && typeof val === 'object') {
-        return Object.getPrototypeOf(val).constructor;
+    let valType: Type;
+    switch (typeof val[0]) {
+        case 'object':
+            valType = val ? Object.getPrototypeOf(val).constructor : undefined;
+            break;
+        case 'string':
+            valType = String;
+            break;
+        case 'number':
+            valType = Number;
+            break;
+        case 'bigint':
+            valType = BigInt;
+            break;
+        case 'boolean':
+            valType = Boolean;
+            break;
+        case 'symbol':
+            valType = Symbol;
+            break;
+        case 'function':
+        case 'undefined':
+            valType = undefined;
     }
+    if (arr) {
+        return [valType];
+    }
+    return valType;
+}
+
+/**
+ * Gets the constructor for the specified property, if it is an object type. Returns undefined if
+ * the specified property is primitive or non-existent.
+ * @param obj - the parent object or its constructor
+ * @param key - the property key
+ * @param context - context object for filtering decorators
+ * @returns Constructor | undefined - the constructor function for the specified property
+ */
+export function getPropConstructor(obj: object | Constructor, key: string | symbol, context?: { [k: string]: any }): Constructor | undefined {
+    let propCtor = getPropType(obj, key, context);
+    
+    // in case of array, use first element as sample
+    if (Array.isArray(propCtor)) {
+        propCtor = propCtor[0];
+    }
+
+    if (typeof propCtor === 'function') {
+        if (
+            propCtor !== String && 
+            propCtor !== Boolean && 
+            propCtor !== Number && 
+            propCtor !== BigInt && 
+            propCtor !== Symbol
+        ) {
+            return propCtor as Constructor;
+        }
+    }
+    return undefined;
+}
+
+type PropConfig = {
+    type: Type,
+    validate: (val: any) => string | undefined,
+    stringify: (val: any) => string,
+    parse: (str: string) => any,
+};
+
+export function getPropConfig(obj: object | Constructor, key: string | symbol, context?: { [k: string]: any }): PropConfig {
+    const propType = getPropType(obj, key, context);
+
+    let validate: PropConfig['validate'];
+    let stringify: PropConfig['stringify'];
+    let parse: PropConfig['parse'];
+
+    switch (propType) {
+        case String:
+            validate = v => typeof v === 'string' ? '' : 'string required';
+            stringify = v => String(v);
+            parse = v => v;
+            break;
+        case Number:
+            validate = v => typeof v === 'number' && !Number.isNaN(v) ? '' : 'number required';
+            stringify = v => String(v);
+            parse = v => Number.parseFloat(v);
+            break;
+        case BigInt:
+            validate = v => typeof v === 'bigint' ? '' : 'bigint required';
+            stringify = v => String(v);
+            parse = v => {
+                try {
+                    return BigInt(v);
+                } catch (_) {
+                    return undefined;
+                }
+            };
+            break;
+        case Boolean:
+            validate = v => typeof v === 'boolean' ? '' : 'boolean required';
+            stringify = v => v ? 'true' : '';
+            parse = v => Boolean(v);
+            break;
+        case Symbol:
+            validate = v => typeof v === 'symbol' ? '' : 'symbol required';
+            stringify = v => String(v);
+            parse = v => { throw new Error(`cannot convert string to symbol: ${v}`); };
+            break;
+        case Date:
+            validate = v => v instanceof Date ? '' : 'date required';
+            stringify = v => (v as Date).toISOString();
+            parse = v => new Date(v);
+        case undefined:
+            validate = v => typeof v === 'undefined' ? '' : 'undefined required';
+            stringify = v => String(v);
+            // We could just return undefined here. But if this function ever gets called,
+            // there's probably something wrong.
+            parse = v => { throw new Error(`cannot convert string to undefined: ${v}`); };
+            break;
+        default:
+            if (Array.isArray(propType)) {
+                validate = v => Array.isArray(v) ? '' : 'array required';
+                stringify = JSON.stringify;
+                parse = JSON.parse;
+            } else {
+                validate = v => Object.getPrototypeOf(v).constructor === propType 
+                    ? '' 
+                    : `invalid ${propType.name}`;
+                stringify = JSON.stringify;
+                parse = v => {
+                    try {
+                        const entries = convertToEntriesRecursively(JSON.parse(v));
+                        return createRecursively(propType as Constructor, entries);
+                    } catch (_) {
+                        return undefined;
+                    }
+                };
+            }
+    }
+
+    return {
+        type: propType,
+        validate,
+        stringify,
+        parse,
+    };
+}
+
+export function convertToEntries(obj: object): [string | symbol | number, any][] {
+    if (typeof (obj as any).toEntries === 'function') {
+        return (obj as any).toEntries();
+    }
+    // While a custom method `toEntries` (see above) may include symbol keys, the default
+    // implementation does not. Hence the function return type is broader than the type of the
+    // following array.
+    const entries: [string | number, any][] = [];
+    if (Array.isArray(obj)) {
+        for (let i = 0; i < obj.length; i++) {
+            entries.push([i, obj[i]]);
+        }
+    } else {
+        for (const k in obj) {
+            // exclude methods
+            if (typeof (obj as any)[k] !== 'function') {
+                entries.push([k, (obj as any)[k]]);
+            }
+        }
+    }
+    return entries;
+}
+
+export function convertToEntriesRecursively(obj: object): [(string | symbol | number)[], any][] {
+    const entries: [(string | symbol | number)[], any][] = [];
+    for (const [key, value] of convertToEntries(obj)) {
+        if ((obj as any)[key] && typeof (obj as any)[key] === 'object') {
+            const childEntries = convertToEntriesRecursively(value);
+            for (const [keyTail, simpleValue] of childEntries) {
+                entries.push([[key, ...keyTail], simpleValue]);
+            }
+        } else {
+            entries.push([[key], value]);
+        }
+    }
+    return entries;
 }
 
 export function createFromEntries<T>(ctor: Constructor<T>, entries: [string | symbol, any][]): T | undefined {
