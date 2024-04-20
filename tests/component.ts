be81efce0b718e497a67521b431c3875a56fdf19
dtrlanz <68445623+dtrlanz@users.noreply.github.com>
2024-04-20 16:55:16 +0500

    test construction with array

--- tests/component.ts
+++ tests/component.ts
@@ -74,3 +74,115 @@ test('simple object structure', t => {
         }
     ]);
 });
+
+test('array of objects', t => {
+    class A {
+        @title('Foo')
+        foo = 3.14;
+
+        @title('Bar')
+        bar = false;
+    }
+
+    let objA = new A();
+    let comp: Component<any> = new Component([objA, objA]);
+    let rowTitles = [...new Set(comp['children'].map(c => c.title))];
+    t.deepEqual(getUiStructure(comp['children']), [
+        {
+            title: rowTitles[0],
+            children: [
+                {
+                    title: 'Foo',
+                    keyTuple: [0, 'foo'],
+                },
+                {
+                    title: 'Bar',
+                    keyTuple: [0, 'bar'],
+                },        
+            ]
+        },
+        {
+            title: rowTitles[1],
+            children: [
+                {
+                    title: 'Foo',
+                    keyTuple: [1, 'foo'],
+                },
+                {
+                    title: 'Bar',
+                    keyTuple: [1, 'bar'],
+                },        
+            ]
+        },
+    ]);
+
+    t.deepEqual(comp['header'], [
+        ['Foo'],
+        ['Bar'],
+    ]);
+
+    class B {
+        @title('Apples')
+        a = objA;
+
+        @title('Oranges')
+        b = 42;
+    }
+
+    const objB = new B();
+    comp = new Component([objB, objB]);
+    rowTitles = [...new Set(comp['children'].map(c => c.title))];
+    t.deepEqual(getUiStructure(comp['children']), [
+        {
+            title: rowTitles[0],
+            children: [
+                {
+                    title: 'Apples',
+                    children: [
+                        {
+                            title: 'Foo',
+                            keyTuple: [0, 'a', 'foo'],
+                        },
+                        {
+                            title: 'Bar',
+                            keyTuple: [0, 'a', 'bar'],
+                        },        
+                    ]
+                },
+                {
+                    title: 'Oranges',
+                    keyTuple: [0, 'b'],
+                },        
+            ]
+        },
+        {
+            title: rowTitles[1],
+            children: [
+                {
+                    title: 'Apples',
+                    children: [
+                        {
+                            title: 'Foo',
+                            keyTuple: [1, 'a', 'foo'],
+                        },
+                        {
+                            title: 'Bar',
+                            keyTuple: [1, 'a', 'bar'],
+                        },        
+                    ]
+                },
+                {
+                    title: 'Oranges',
+                    keyTuple: [1, 'b'],
+                },        
+            ]
+        },
+    ]);
+
+    t.deepEqual(comp['header'], [
+        ['Apples', 'Foo'],
+        ['Apples', 'Bar'],
+        ['Oranges'],
+    ]);
+
+});
